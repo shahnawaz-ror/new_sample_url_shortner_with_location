2d1c8fcc4af27fa1fe4ba7b1d623a495a7f1513e
mohammad shahnawaz <shahnawaz.spaceo@gmail.com>
2021-02-14 00:57:58 +0530

    Updated URL for default in dot env.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   resources :url_shorts
   devise_for :users
   get '/:id' => "shortener/shortened_urls#show"
-  root 'home#index'
-  # get 'home/stats',
+  root 'home#stats'
   get '/home/stats' ,to: 'home#stats'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
